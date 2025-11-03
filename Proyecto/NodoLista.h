7f50243708c75b4bc32176ad500043e1407ab191
Eusse7 <afeussec@eafit.edu.co>
2025-11-03 17:19:12 -0500

    Update NodoLista.h

--- Proyecto/NodoLista.h
+++ Proyecto/NodoLista.h
@@ -1,18 +1,12 @@
-#pragma once
-
-#include "Ingrediente.h"
-
-using namespace std;
-
 class NodoLista {
 private:
-    Ingrediente dato;
-    NodoLista* siguiente;
+    Ingrediente dato;
+    NodoLista* siguiente;
 
 public:
-    NodoLista(Ingrediente d) : dato(d), siguiente(nullptr) {}
-    
-    Ingrediente getDato() const { return dato; }
-    NodoLista* getSiguiente() const { return siguiente; }
-    void setSiguiente(NodoLista* sig) { siguiente = sig; }
+    NodoLista(Ingrediente d) : dato(d), siguiente(nullptr) {}
+    
+    Ingrediente getDato() const { return dato; }
+    NodoLista* getSiguiente() const { return siguiente; }
+    void setSiguiente(NodoLista* sig) { siguiente = sig; }
 };
