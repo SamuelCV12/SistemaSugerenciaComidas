52e4172bc66f2d476c64745fe1df67c2cc56f653
Eusse7 <afeussec@eafit.edu.co>
2025-11-03 17:17:57 -0500

    Refactor NodoArbol class structure

--- Proyecto/NodoArbol.h
+++ Proyecto/NodoArbol.h
@@ -1,24 +1,17 @@
-#pragma once
-
-#include "Receta.h"
-
-using namespace std;
-
-// ==================== NODO ÁRBOL BST ====================
 class NodoArbol {
 private:
-    Receta receta;
-    NodoArbol* izquierdo;
-    NodoArbol* derecho;
+    Receta receta;
+    NodoArbol* izquierdo;
+    NodoArbol* derecho;
 
 public:
-    NodoArbol(Receta r) : receta(r), izquierdo(nullptr), derecho(nullptr) {}
-    
-    Receta& getReceta() { return receta; }
-    const Receta& getReceta() const { return receta; }
-    NodoArbol* getIzquierdo() const { return izquierdo; }
-    NodoArbol* getDerecho() const { return derecho; }
-    
-    void setIzquierdo(NodoArbol* izq) { izquierdo = izq; }
-    void setDerecho(NodoArbol* der) { derecho = der; }
+    NodoArbol(Receta r) : receta(r), izquierdo(nullptr), derecho(nullptr) {}
+    
+    Receta& getReceta() { return receta; }
+    const Receta& getReceta() const { return receta; }
+    NodoArbol* getIzquierdo() const { return izquierdo; }
+    NodoArbol* getDerecho() const { return derecho; }
+    
+    void setIzquierdo(NodoArbol* izq) { izquierdo = izq; }
+    void setDerecho(NodoArbol* der) { derecho = der; }
 };
